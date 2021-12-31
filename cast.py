268ffe128210a1c0043317009521e580005af7ec
mutyola <mutyola3@gmail.com>
2021-12-31 22:37:34 +0200

    added addition to cast.py

--- cast.py
+++ cast.py
@@ -1,5 +1,11 @@
 #input numbers
 num1=input('Please enter a whole number:')
-num2=input('Now enter another whole number')
+num2=input('Now enter another whole number:')
 print('Input is:',type(num1),type(num2))
+
+#addition
+total=num1+num2
+print('Total:',total,type(total))
+
+
 
