6a0a6b3cc1aabbc86af8e81500419da71442e0b3
mutyola <mutyola3@gmail.com>
2022-01-01 22:58:21 +0200

    added compare to  guess.py

--- guess.py
+++ guess.py
@@ -5,4 +5,14 @@ flag=True
 guess=0
 #display message for  user guess
 print('Guess my number 1-20:',end='')
-
+#compare
+while flag==True:
+guess=input()
+if not guess.isdigit():
+print('Invalid!Enter only digits 1-20')
+break
+elif int(guess)<num:
+print('Too low,try again:',end='')
+else:
+print('Correct...My number is'+guess)
+flag=False
