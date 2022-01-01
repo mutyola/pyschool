c5da71bb5844a6355fb6fbdcbb3e9c10c93f12be
mutyola <mutyola3@gmail.com>
2022-01-01 23:11:16 +0200

    editted guess.py

--- guess.py
+++ guess.py
@@ -5,8 +5,10 @@ flag=True
 guess=0
 #display message for  user guess
 print('Guess my number 1-20:',end='')
+
 #compare
 while flag==True:
+
 guess=input()
 if not guess.isdigit():
 print('Invalid!Enter only digits 1-20')
