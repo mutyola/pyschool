07ec3df0f72f03c63079261945bb00e6025e83d2
mutyola <mutyola3@gmail.com>
2022-01-09 03:31:21 +0200

    added comments to BMI

--- BMI.py
+++ BMI.py
@@ -1,12 +1,14 @@
 #your code goes here
-weight = int(input());
-height = float(input());
+print('Welcome')
+print('This is your Body Mass Index Calculator!')
+weight = int(input('Enter your weight:'));
+height = float(input('Enter your height:'));
 x = weight/float(height*height);
 if x < 18.5:
-    print('Underweight')
+    print('You are Underweight.')
 if x>=18.5 and x<25:
-    print("Normal")
+    print("You are Normal.")
 if x >= 25 and x < 30:
-   print('Overweight')
+   print('You are Overweight.')
 if x >= 30:
-   print('Obesity')
+   print('You are Obese.')
