9979544de68215f6b80d8b3b16f306926202374e
mutyola <mutyola3@gmail.com>
2022-01-13 19:24:30 +0200

    Added more statements to pop.py

--- pop.py
+++ pop.py
@@ -1,4 +1,10 @@
 basket = [ 'Apple' , 'Bun' , 'Cola' ]
 crate = [ 'Egg' , 'Fig' , 'Grape' ]
 print( 'Basket List:' , basket )
-print( 'Basket Elements:' , len( basket ) )
+print( 'Basket Elements:' , len( basket ))
+basket.extend( crate )
+print( 'Extended:' , basket )
+del basket[1]
+print( 'Item Removed:' , basket )
+del basket[1:3]
+print( 'Slice Removed:' , basket )
