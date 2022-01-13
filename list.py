42f6bebefd358197306f8f0fd2bbcdbc8f85ae73
mutyola <mutyola3@gmail.com>
2022-01-13 19:12:12 +0200

    Added statement to list.py

--- list.py
+++ list.py
@@ -2,4 +2,4 @@ quarter = [ ‘January’ , ‘February’ , ‘March’ ]
 print( ‘First Month :’ , quarter[0] )
 print( ‘Second Month :’ , quarter[1] )
 print( ‘Third Month :’ , quarter[2] )
-
+coords = [ [ 1 , 2 , 3 ] , [ 4 , 5 , 6 ] ]
