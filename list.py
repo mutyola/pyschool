fc0d317f8c624433957242f3fba5e1bc206c5242
mutyola <mutyola3@gmail.com>
2022-01-13 19:14:08 +0200

    Added final statements to list.py

--- list.py
+++ list.py
@@ -5,3 +5,4 @@ print( ‘Third Month :’ , quarter[2] )
 coords = [ [ 1 , 2 , 3 ] , [ 4 , 5 , 6 ] ]
 print( ‘\nTop Left 0,0 :’ , coords[0][0] )
 print( ‘Bottom Right 1,2 :’ , coords[1][2] )
+print( ‘\nSecond Month First Letter :’ , quarter[1][0] )
