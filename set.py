1bf484b2a8786f9c09381bf478037530620b8ac9
mutyola <mutyola3@gmail.com>
2022-01-14 07:50:00 +0200

    Added statements to set.py

--- set.py
+++ set.py
@@ -1,2 +1,4 @@
 party_goers = { 'Andrew' , 'Barbara' , 'Carole' , 'David' }
 print( 'party_goers:' , type( party_goers ) )
+print( 'Did David go to the party?' , 'David' in party_goers )
+print( 'Did Kelly go to the party?' , 'Kelly' in party_goers )
