c5a0b9501550cce6f3a38dc3853d5b5461df466d
mutyola <mutyola3@gmail.com>
2022-01-14 07:52:09 +0200

    Added commons set to set.py

--- set.py
+++ set.py
@@ -3,3 +3,4 @@ print( 'party_goers:' , type( party_goers ) )
 print( 'Did David go to the party?' , 'David' in party_goers )
 print( 'Did Kelly go to the party?' , 'Kelly' in party_goers )
 students = { 'Andrew' , 'Kelly' , 'Lynn' , 'David' }
+commons = party_goers.intersection( students )
