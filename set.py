50c18ec59829b934b69cb8c61a4d58d35eee9ec5
mutyola <mutyola3@gmail.com>
2022-01-14 07:53:17 +0200

    Added regular set to set.py

--- set.py
+++ set.py
@@ -4,3 +4,4 @@ print( 'Did David go to the party?' , 'David' in party_goers )
 print( 'Did Kelly go to the party?' , 'Kelly' in party_goers )
 students = { 'Andrew' , 'Kelly' , 'Lynn' , 'David' }
 commons = party_goers.intersection( students )
+party_students = list( commons )
