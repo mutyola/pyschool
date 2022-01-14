bb1881d5ba1d7911c705397e5c080ea8007b558b
mutyola <mutyola3@gmail.com>
2022-01-14 07:40:55 +0200

    Added another turple to turple.py

--- turple.py
+++ turple.py
@@ -3,3 +3,5 @@ print( 'days:' , type( days ) )
 print( 'Days of the week:' , days )
 print( 'No. of days in week:' , len( days ) )
 print( 'Start day of week:' , days[0] )
+user = ( 'John' , 'Doe' , 'Paris' , '555-1234' )
+
