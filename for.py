2888cfe57e2ae493fb6006029b7fd5752a90d818
mutyola <mutyola3@gmail.com>
2022-01-15 18:37:44 +0200

    Added statement to display var

--- for.py
+++ for.py
@@ -1,3 +1,6 @@
 chars = [ 'A' , 'B', 'C' ]
 fruit = ( 'Apple' , 'Banana' , 'Cherry' )
 info = { 'name' : 'Mike' , 'ref' : 'Python' , 'sys' : 'Win' }
+print( 'Elements: \t' , end = ' ' )
+for item in chars :
+print( item , end = ' ' )
