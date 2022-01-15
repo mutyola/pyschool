810da020a7964fb6f6fd93c453fd0b581a1ae509
mutyola <mutyola3@gmail.com>
2022-01-15 18:14:45 +0200

    Added second var test and display to if.py

--- if.py
+++ if.py
@@ -4,3 +4,8 @@ print( 'Number Exceeds 5' )
 elif num < 5 :print( 'Number is Less Than 5' )
 else :
 print( 'Number Is 5' )
+if num > 7 and num < 9 :
+print( 'Number is 8' )
+if num == 1 or num == 3 :
+print( 'Number Is 1 or 3' )
+
