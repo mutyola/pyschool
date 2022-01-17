a2cda2e8c9047cf21bbd93d0e6fbfd4bf9389bea
mutyola <mutyola3@gmail.com>
2022-01-17 12:43:18 +0200

    Added continue statement to continue.py

--- continue.py
+++ continue.py
@@ -4,3 +4,6 @@ for i in range( 1, 4 ) :
 	if i == 2 and j == 1 :
 		print( 'Breaks inner loop at i=2 j=1' )
 	break
+	if i == 1 and j == 1 :
+		print( 'Continues inner loop at i=1 j=1' )
+		continue
