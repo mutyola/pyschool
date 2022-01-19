208e9cf53c09827200321bb3dfd37a3dc8289d2d
mutyola <mutyola3@gmail.com>
2022-01-19 15:35:21 +0200

    Added call to  functions to param.py

--- param.py
+++ param.py
@@ -4,3 +4,7 @@ echo( 'Mike' , 'Python' , 'Windows' )
 echo( lang = 'Python' , sys = 'Mac OS' , user = 'Anne' )
 def mirror( user = 'Carole' , lang = 'Python' ) :
 	print( '\nUser:' , user , 'Language:' , lang )
+mirror()
+mirror( lang = 'Java' )
+mirror( user = 'Tony' )
+mirror( 'Susan' , 'C++' )
