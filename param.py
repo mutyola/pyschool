dc4a9184deffc308464750de02accbda0de9e19f
mutyola <mutyola3@gmail.com>
2022-01-19 15:31:42 +0200

    Added 2nd call to function to param.py

--- param.py
+++ param.py
@@ -1,3 +1,4 @@
 def echo( user , lang , sys ) :
 	print( 'User:', user, 'Language:', lang, 'Platform:', sys )
 echo( 'Mike' , 'Python' , 'Windows' )
+echo( lang = 'Python' , sys = 'Mac OS' , user = 'Anne' )
