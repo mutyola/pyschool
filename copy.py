d515ea1fb0de1178dd93ce8621cf29ffdc92cb7f
mutyola <mutyola3@gmail.com>
2022-01-21 16:34:07 +0200

    Editted copy.py

--- copy.py
+++ copy.py
@@ -8,10 +8,10 @@ while len( copy ) > 0 :
 for element in range( 0 , len( copy ) ) :
 	if copy[ element ] < copy[ minimum ] :
 		minimum = element
-print( '\tRemoving value' , copy[ minimum ] , \
+	print( '\tRemoving value' , copy[ minimum ] , \
 			'from' , copy )
 sorted_copy.append( copy.pop( minimum )
-array = [ 5 , 3 , 1 , 2 , 6 , 4 ]
-print( 'Copy Sort...\nArray :' , array )
-print( 'Copy :' , copy_sort( array ) )
-print( 'Array :' , array )
+	array = [ 5 , 3 , 1 , 2 , 6 , 4 ]
+	print( 'Copy Sort...\nArray :' , array )
+	print( 'Copy :' , copy_sort( array ) )
+	print( 'Array :' , array )
