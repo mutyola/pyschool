dc456caa0f8e1e0ee78d22d2e3ca0c208768e809
mutyola <mutyola3@gmail.com>
2022-01-21 15:58:39 +0200

    Added indented algorithm sequence to copy.py

--- copy.py
+++ copy.py
@@ -3,3 +3,11 @@ def copy_sort( array ) :
 	sorted_copy = [ ]
 	# Algorithm sequence to be added here.
 	return sorted_copy
+while len( copy ) > 0 :
+	minimum = 0
+for element in range( 0 , len( copy ) ) :
+	if copy[ element ] < copy[ minimum ] :
+		minimum = element
+print( '\tRemoving value' , copy[ minimum ] , \
+			'from' , copy )
+sorted_copy.append( copy.pop( minimum )
