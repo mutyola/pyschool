fe79d35b62f5267769e28b881246aa0ccc8a3155
mutyola <mutyola3@gmail.com>
2022-01-24 22:29:18 +0200

    Added function fibonacci

--- function.py
+++ function.py
@@ -6,3 +6,10 @@ def calculate(val_a, val_b):
 def perform(num):
 	d = num * 5
 	return d, d + 5, d - 2
+def fibonacci(n):
+	if n == 0:
+		return 0
+	elif n == 1:
+		return 1
+	else:
+		return fibonacci(n-1) + fibonacci(n-2)
