42ad9f65ed59737ff82ed2d91edfa87d7f102717
mutyola <mutyola3@gmail.com>
2022-01-24 23:19:33 +0200

    Added class circle to inherit.py

--- inherit.py
+++ inherit.py
@@ -5,3 +5,11 @@ class shape:
 		print('call __init__ from shape class')
 	def foo(self):
 		print('calling foo() from shape class')
+
+class circle(shape):
+	def __init__(self, r):
+		print('call __init__ from circle class')
+		self.r = r
+	def calculate_area_circle(self):
+		return math.pi * self.r * self.r
+
