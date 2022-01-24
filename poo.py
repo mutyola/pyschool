deabff2695a3710dc1c3d9ba8774a1eb991b18fa
mutyola <mutyola3@gmail.com>
2022-01-24 23:03:02 +0200

    Added class city attributes to poo.py

--- poo.py
+++ poo.py
@@ -4,3 +4,11 @@ class City:
 	city_count = 0
 	city_id = 0
 	### do something
+	# class attributes
+	def move_to(self, x=0, y=0):
+		self.x += x
+		self.y += y
+	def distance(self, other_city):
+		xi = pow(other_city.x - self.x, 2)
+		yi = pow(other_city.y - self.y, 2)
+		return sqrt(xi + yi)
