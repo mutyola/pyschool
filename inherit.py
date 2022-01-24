869d910bb729ae66f3e307faf6b89dc91e920fd1
mutyola <mutyola3@gmail.com>
2022-01-24 23:21:24 +0200

    Added class rectangle to inherit.py

--- inherit.py
+++ inherit.py
@@ -13,3 +13,11 @@ class circle(shape):
 	def calculate_area_circle(self):
 		return math.pi * self.r * self.r
 
+class rectangle(shape):
+	def __init__(self, l, w):
+		print('call __init__ from rectangle class')
+		self.l = l
+		self.w = w
+	def calculate_area_rectangle(self):
+		return self.l * self.w
+
