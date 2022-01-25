c6664bcf376071995026fdbb467e254825bc7845
mutyola <mutyola3@gmail.com>
2022-01-25 22:11:54 +0200

    Added function to class

--- overide.py
+++ overide.py
@@ -10,3 +10,6 @@ class circle(shape):
 	def __init__(self, r):
 		print('call __init__ from circle class')
 		self.r = r
+	def calculate_area(self):
+		print('calling calculate_area() from circle class')
+		return math.pi * self.r * self.r
