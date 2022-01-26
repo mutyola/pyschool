8a540fd3966631d4a7d72862c46b635b923ae714
mutyola <mutyola3@gmail.com>
2022-01-26 17:58:19 +0200

    Added typo, syntax corrections

--- overload.py
+++ overload.py
@@ -19,6 +19,7 @@ class Point:
     def __str__(self):
         return 'x' + str(self.x) + ', y:' + str(self.y)
 
+
 a = Point(10, 3)
 b = Point(2, 7)
 c = Point(8, 1)
@@ -26,4 +27,4 @@ c = Point(8, 1)
 print(a)
 print(a + b)
 print(c - b)
-print(a * c
+print(a * c)
