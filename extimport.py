5099bbdc8e438a2ab58ae03ff5737ba8fbee7c2c
mutyola <mutyola3@gmail.com>
2022-01-27 19:58:45 +0200

    Added display statements

--- extimport.py
+++ extimport.py
@@ -10,3 +10,9 @@ from colorama import Fore, Back, Style
 
 colorama.init()
 message = "hello world from python"
+
+print(message)
+print(Fore.RED + message)
+print(Fore.GREEN + message)
+print(Fore.BLUE + message)
+print(Fore.RED + Back.YELLOW + message + Style.RESET_ALL)
