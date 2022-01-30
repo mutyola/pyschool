af71c2d0dcb78014e4d82ae74bd55f98968101a4
mutyola <mutyola3@gmail.com>
2022-01-30 20:37:22 +0200

    Added concatenation to wstrings

--- wstrings.py
+++ wstrings.py
@@ -1,3 +1,9 @@
 # string operations
 str1 = "hello world"
 str2 = "python"
+
+# Concatenating
+print(str1 + " " + str2)
+print(str1, str2)
+print("%s %s" % (str1, str2))
+print("{} {}".format(str1, str2))
