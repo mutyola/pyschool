0ac83f83945e7def7ef4610d8786b576b8346ffa
mutyola <mutyola3@gmail.com>
2022-01-31 20:24:11 +0200

    Added corrections  to WDF

--- WDF.py
+++ WDF.py
@@ -1,9 +1,23 @@
+
+# writing file
+
+#####################################
+print('creating files...')
+
+# create a file.
+# If file is existing, it erases and creates a new one
+f1 = open('mydoc1', 'w')
+
+
+# create a file.
 # If file is existing, it appends. Otherwise, it creates
 f2 = open('mydoc2', 'a')
+
 
 # binary files
 bf1 = open('mydoc3', 'wb')
 bf2 = open('mydoc4', 'ab')
+
 
 #####################################
 # writing data
@@ -23,7 +37,8 @@ for index in range(1, 12):
     bf1.write(data)
     bf2.write(data)
 
-    #####################################
+
+#####################################
 # close all
 
 print('close files...')
