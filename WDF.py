3fe7a70ce2f5710e89f424dc6d9393f855319cc5
mutyola <mutyola3@gmail.com>
2022-01-31 20:16:18 +0200

    Added data writer to WDF

--- WDF.py
+++ WDF.py
@@ -4,3 +4,21 @@ f2 = open('mydoc2', 'a')
 # binary files
 bf1 = open('mydoc3', 'wb')
 bf2 = open('mydoc4', 'ab')
+
+#####################################
+# writing data
+
+print('writing data into files...')
+for index in range(1, 12):
+    data = ''
+    name = 'user ' + str(index-1)
+    email = 'user' + str(index-1) + '@email.com'
+    if index == 1:
+        data = '{0:3s} {1:10s} {2:15s}\n'.format('No', 'Name', 'Email')
+    else:
+        data = '{0:3s} {1:10s} {2:15s}\n'.format(str(index-1), name, email)
+
+    f1.write(data)
+    f2.write(data)
+    bf1.write(data)
+    bf2.write(data)
