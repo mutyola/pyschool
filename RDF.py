e2b2c63041e99334b7e53e83458f3f2df44e9f26
mutyola <mutyola3@gmail.com>
2022-02-01 21:28:06 +0200

    Added reading data function to RDF

--- RDF.py
+++ RDF.py
@@ -9,3 +9,14 @@ f1 = open('mydoc1', 'r')
 f2 = open('mydoc2', 'r')
 bf1 = open('mydoc3', 'rb')
 bf2 = open('mydoc4', 'rb')
+
+#####################################
+# reading data
+
+def reading_data(f):
+    while True:
+        data = f.readline()
+        if (data == '') or (data == None):
+            break
+
+        sys.stdout.write(data)
