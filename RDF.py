2a191edd37b595ac354ce8b66f52df2800d80cf3
mutyola <mutyola3@gmail.com>
2022-02-01 21:26:56 +0200

    Added functions to RDF

--- RDF.py
+++ RDF.py
@@ -1,3 +1,11 @@
 # reading file
 
 import sys
+
+#####################################
+print('opening files...')
+
+f1 = open('mydoc1', 'r')
+f2 = open('mydoc2', 'r')
+bf1 = open('mydoc3', 'rb')
+bf2 = open('mydoc4', 'rb')
