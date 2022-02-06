95a41b0a1e5ffe5c4534d38dfa7323f5bbb609a8
mutyola <mutyola3@gmail.com>
2022-02-06 21:36:54 +0200

    Added str function

--- customexcept.py
+++ customexcept.py
@@ -5,4 +5,8 @@ class MySimpleError(Exception):
     def __init__(self, code, message):
         self.code = code
         self.message = message
-        
+
+    def __str__(self):
+        return repr(str(self.code) + ":" + self.message)
+
+    
