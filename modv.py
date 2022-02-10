d852d405ffbb46c99a877475b4d45850cf4158c8
mutyola <mutyola3@gmail.com>
2022-02-10 23:10:05 +0200

    Added class attributes

--- modv.py
+++ modv.py
@@ -4,7 +4,7 @@ class City:
     # class data
     city_count = 0
     city_id = 0
-    
+
      # constructor
     def __init__(self, name='', x=0, y=0):
         self.name = name
@@ -15,3 +15,19 @@ class City:
 
     def __str__(self):
         return 'City: ' + self.name + ',id=' + str(self.city_id) + ',x=' + str(self.x) + ',y=' + str(self.y)
+
+    # class attributes
+    def move_to(self, x=0, y=0):
+        self.x += x
+        self.y += y
+
+    def distance(self, other_city):
+        xi = pow(other_city.x - self.x, 2)
+        yi = pow(other_city.y - self.y, 2)
+
+        return sqrt(xi + yi)
+
+    def __del__(self):
+        # get class name
+        class_name = self.__class__.__name__
+        print('class ', class_name, ' destroyed')
